35fa86ef04a2a3e79151fe49f88234a7fc25eec0
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-03-02 00:36:18 +0100

    Update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,75 +1,52 @@
 #include "binary_trees.h"
-
-unsigned char is_terminal(const binary_tree_t *node);
-size_t measure_depth(const binary_tree_t *tree);
-const binary_tree_t *fetch_terminal(const binary_tree_t *tree);
-int is_perfect_recursive(const binary_tree_t *tree,
-		size_t terminal_depth, size_t level);
-int check_perfection(const binary_tree_t *tree);
-
 /**
- * is_terminal - Determines if a node is a terminal node in a binary tree.
- * @node: A pointer to the node to be checked.
- *
- * Return: 1 if the node is a terminal node, 0 otherwise.
+ * tree_is_perfect - function that says if a tree is perfect or not
+ * it has to be the same quantity of levels in left as right, and also
+ * each node has to have 2 nodes or none
+ * @tree: tree to check
+ * Return: 0 if is not a perfect or other number that is the level of height
  */
-unsigned char is_terminal(const binary_tree_t *node)
+int tree_is_perfect(const binary_tree_t *tree)
 {
-	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
-}
-
-/**
- * easure_depth - Calculates the depth of a specified node in a binary tree.
- * @tree: A pointer to the node whose depth is to be calculated.
- *
- * Return: The depth of the specified node.
- */
-size_t measure_depth(const binary_tree_t *tree)
-{
-	return (tree->parent != NULL ? 1 + measure_depth(tree->parent) : 0);
-}
-
-/**
- * fetch_terminal - Retrieves a terminal node from a binary tree.
- * @tree: A pointer to the root node of the binary tree.
- *
- * Return: A pointer to the first terminal node encountered.
- */
-const binary_tree_t *fetch_terminal(const binary_tree_t *tree)
-{
-	if (is_terminal(tree) == 1)
-		return (tree);
-	return (tree->left ? fetch_terminal(tree->left) : fetch_terminal(tree->right));
-}
-
-/**
- * is_perfect_recursive - Determines if a binary tree is perfect recursively.
- * @tree: A pointer to the root node of the binary tree.
- * @terminal_depth: The depth of a terminal node in the binary tree.
- * @level: The level of the current node.
- *
- * Return: 1 if the binary tree is perfect, 0 otherwise.
- */
-int is_perfect_recursive(const binary_tree_t *tree,
-	size_t terminal_depth, size_t level)
-{
-	if (is_terminal(tree))
-		return (level == terminal_depth ? 1 : 0);
-	if (tree->left == NULL || tree->right == NULL)
+	int l = 0, r = 0;
+	
+	if (tree->left && tree->right)
+	{
+		l = 1 + tree_is_perfect(tree->left);
+		r = 1 + tree_is_perfect(tree->right);
+		if (r == l && r != 0 && l != 0)
+			return (r);
 		return (0);
-	return (is_perfect_recursive(tree->left, terminal_depth, level + 1) &&
-			is_perfect_recursive(tree->right, terminal_depth, level + 1));
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
 /**
- * binary_tree_is_perfect - Determines if a binary tree is perfect.
- * @tree: A pointer to the root node of the binary tree.
- *
- * Return: 0 if the binary tree is NULL or not perfect, 1 otherwise.
+ * binary_tree_is_perfect - perfect or not a tree
+ * @tree: tree to check
+ * Return: 1 is it is or 0 if not
  */
-int check_perfection(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int result = 0;
+	
 	if (tree == NULL)
+	{
 		return (0);
-	return (is_perfect_recursive(tree, measure_depth(fetch_terminal(tree)), 0));
+	}
+	else
+	{
+		result = tree_is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
+		return (0);
+	}
 }
